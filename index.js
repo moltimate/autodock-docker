98a55e851150dd12d0d40b5f69026d040d5aa99b
Tyler Reimold <tylerreimold@gmail.com>
2019-11-11 00:17:05 -0500

    MOL-29 Implement basic node functionality This commit adds the ability to process certain commands and the ability to take files from the api and give them a cryptographically secure name. It also adds a line to the dockerfile giving vina execute permissions.

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
 const express = require('express');
+const crypto = require('crypto');
+var exec = require('child_process').execFile;
 const app = express();
-
-app.get('/', (req, res) => {
+var formidable = require('formidable');
+app.use(express.json())
+app.get('/v1/autodock', (req, res) => {
     res.send('Hello World!')
+});
+
+app.post('/v1/autodock', (req, res) => {
+    ligend = {}
+    macromolecule = {}
+    fields = {}
+    var form = new formidable.IncomingForm();
+    form.multiples = true;
+    form.parse(req);
+    form.on('field', function(name, value) {
+        fields[name] = value
+    })
+    form.on('fileBegin', function (name, file){
+        let nameHash = crypto.createHmac('sha1', crypto.randomBytes(48))
+        .update(Date.now()
+        .toString())
+        .digest('hex')
+        if (name == 'ligend') {
+            ligend.originalName = name
+            ligend.name = nameHash
+        }
+        else if (name == 'macromolecule') {
+            macromolecule.originalName = name
+            macromolecule.name = nameHash
+        }
+        else {
+            res.status(400);
+            res.send('Unknown file name parameter: ' + name);
+        }
+        file.path = __dirname + '/uploads/' + nameHash + '.pdbqt';
+    });
+    form.on('end', function() {
+        try {
+            argsString = 
+            ' --receptor ' +  __dirname + '/uploads/' + macromolecule.name + '.pdbqt' +  
+            ' --ligend ' + __dirname + '/uploads/' + ligend.originalName + '.pdbqt'
+        }
+        catch(err) {
+            res.status(400)
+            res.send('Incorrect arguments provided.')
+        }
+        try {
+            exec(`${__dirname}/vina ` + argsString, function(error, stdout, stderr) {
+                console.log(stdout)
+                console.log(stderr)
+                console.log(error)
+            });
+        }
+        catch(error) {
+            res.status(400)
+            res.send('Execution error: ' + error)
+        }
+    });
+
 });
 
 app.listen(8000, () => {
